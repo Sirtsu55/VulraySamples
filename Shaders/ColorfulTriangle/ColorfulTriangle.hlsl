1097070a6de54cc85a509c670181f76f1a45e135
Sirtsu55 <65868911+Sirtsu55@users.noreply.github.com>
2023-06-30 11:41:02 +0300

    fixes

--- Shaders/ColorfulTriangle/ColorfulTriangle.hlsl
+++ Shaders/ColorfulTriangle/ColorfulTriangle.hlsl
@@ -20,14 +20,15 @@ void rgen()
 	uint3 LaunchID = DispatchRaysIndex();
 	uint3 LaunchSize = DispatchRaysDimensions();
 
+	// start with screen space coordinates
 	const float2 pixelCenter = float2(LaunchID.xy) + float2(0.5, 0.5);
 	const float2 inUV = pixelCenter/float2(LaunchSize.xy);
-	float2 d = inUV * 2.0 - 1.0;
-	float4 target = mul(projInverse, float4(d.x, d.y, 1, 1));
+	float2 d = inUV * 2.0 - 1.0; // From [-1, -1] to [1, 1]
+	float4 target = mul(projInverse, float4(d.x, d.y, 1, 1)); // From clip space to view space
 
 	RayDesc rayDesc;
-	rayDesc.Origin = mul(viewInverse, float4(0,0,0,1)).xyz;
-	rayDesc.Direction = mul(viewInverse, float4(normalize(target.xyz), 0)).xyz;
+	rayDesc.Origin = mul(viewInverse, float4(0,0,0,1)).xyz; // extract position from view matrix
+	rayDesc.Direction = mul(viewInverse, float4(normalize(target.xyz), 0)).xyz; // Transform to world space
 	rayDesc.TMin = 0.001;
 	rayDesc.TMax = 10000.0;
 
